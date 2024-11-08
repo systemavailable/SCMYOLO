9851a3fab3469ac19e254f83fa5c2b275a00acfa
systemavailable <143576963+systemavailable@users.noreply.github.com>
2024-11-08 11:02:41 +0800

    Add files via upload

---
+++ model/modules/IRMBCBAM.py
@@ -0,0 +1,255 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from functools import partial
+from einops import rearrange
+from timm.models._efficientnet_blocks import SqueezeExcite
+from timm.models.layers import DropPath
+
+__all__ = ['iRMB_CBAM', 'C2f_iRMB_CBAM']
+
+class ChannelAttention(nn.Module):
+    def __init__(self, in_planes, ratio=16):
+        super(ChannelAttention, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+        self.f1 = nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False)
+        self.relu = nn.ReLU()
+        self.f2 = nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        avg_out = self.f2(self.relu(self.f1(self.avg_pool(x))))
+        max_out = self.f2(self.relu(self.f1(self.max_pool(x))))
+        out = self.sigmoid(avg_out + max_out)
+        return out
+
+
+class SpatialAttention(nn.Module):
+    def __init__(self, kernel_size=7):
+        super(SpatialAttention, self).__init__()
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+        # (特征图的大小-算子的size+2*padding)/步长+1
+        self.conv = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        # 1*h*w
+        avg_out = torch.mean(x, dim=1, keepdim=True)
+        max_out, _ = torch.max(x, dim=1, keepdim=True)
+        x = torch.cat([avg_out, max_out], dim=1)
+        # 2*h*w
+        x = self.conv(x)
+        # 1*h*w
+        return self.sigmoid(x)
+
+class CBAM(nn.Module):
+    def __init__(self, c1, ratio=16, kernel_size=7):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super(CBAM, self).__init__()
+        self.channel_attention = ChannelAttention(c1, ratio)
+        self.spatial_attention = SpatialAttention(kernel_size)
+
+    def forward(self, x):
+        out = self.channel_attention(x) * x
+        # c*h*w
+        # c*h*w * 1*h*w
+        out = self.spatial_attention(out) * out
+        return out
+
+inplace = True
+
+
+class LayerNorm2d(nn.Module):
+
+    def __init__(self, normalized_shape, eps=1e-6, elementwise_affine=True):
+        super().__init__()
+        self.norm = nn.LayerNorm(normalized_shape, eps, elementwise_affine)
+
+    def forward(self, x):
+        x = rearrange(x, 'b c h w -> b h w c').contiguous()
+        x = self.norm(x)
+        x = rearrange(x, 'b h w c -> b c h w').contiguous()
+        return x
+
+
+def get_norm(norm_layer='in_1d'):
+    eps = 1e-6
+    norm_dict = {
+        'none': nn.Identity,
+        'in_1d': partial(nn.InstanceNorm1d, eps=eps),
+        'in_2d': partial(nn.InstanceNorm2d, eps=eps),
+        'in_3d': partial(nn.InstanceNorm3d, eps=eps),
+        'bn_1d': partial(nn.BatchNorm1d, eps=eps),
+        'bn_2d': partial(nn.BatchNorm2d, eps=eps),
+        # 'bn_2d': partial(nn.SyncBatchNorm, eps=eps),
+        'bn_3d': partial(nn.BatchNorm3d, eps=eps),
+        'gn': partial(nn.GroupNorm, eps=eps),
+        'ln_1d': partial(nn.LayerNorm, eps=eps),
+        'ln_2d': partial(LayerNorm2d, eps=eps),
+    }
+    return norm_dict[norm_layer]
+
+
+def get_act(act_layer='relu'):
+    act_dict = {
+        'none': nn.Identity,
+        'relu': nn.ReLU,
+        'relu6': nn.ReLU6,
+        'silu': nn.SiLU
+    }
+    return act_dict[act_layer]
+
+
+class ConvNormAct(nn.Module):
+
+    def __init__(self, dim_in, dim_out, kernel_size, stride=1, dilation=1, groups=1, bias=False,
+                 skip=False, norm_layer='bn_2d', act_layer='relu', inplace=True, drop_path_rate=0.):
+        super(ConvNormAct, self).__init__()
+        self.has_skip = skip and dim_in == dim_out
+        padding = math.ceil((kernel_size - stride) / 2)
+        self.conv = nn.Conv2d(dim_in, dim_out, kernel_size, stride, padding, dilation, groups, bias)
+        self.norm = get_norm(norm_layer)(dim_out)
+        self.act = get_act(act_layer)(inplace=inplace)
+        self.drop_path = DropPath(drop_path_rate) if drop_path_rate else nn.Identity()
+
+    def forward(self, x):
+        shortcut = x
+        x = self.conv(x)
+        x = self.norm(x)
+        x = self.act(x)
+        if self.has_skip:
+            x = self.drop_path(x) + shortcut
+        return x
+
+
+class iRMB_CBAM(nn.Module):
+
+    def __init__(self, dim_in, norm_in=True, has_skip=True, exp_ratio=1.0, norm_layer='bn_2d',
+                 act_layer='relu', v_proj=True, dw_ks=3, stride=1, dilation=1, se_ratio=0.0,
+                 attn_s=True, qkv_bias=False, drop=0., drop_path=0.):
+        super().__init__()
+        dim_out = dim_in
+        self.norm = get_norm(norm_layer)(dim_in) if norm_in else nn.Identity()
+        dim_mid = int(dim_in * exp_ratio)
+        self.has_skip = (dim_in == dim_out and stride == 1) and has_skip
+        self.attn_s = attn_s
+        if self.attn_s:
+            self.cbam = CBAM(dim_in)
+        else:
+            if v_proj:
+                self.v = ConvNormAct(dim_in, dim_mid, kernel_size=1, bias=qkv_bias, norm_layer='none',
+                                     act_layer=act_layer, inplace=inplace)
+            else:
+                self.v = nn.Identity()
+        self.conv_local = ConvNormAct(dim_mid, dim_mid, kernel_size=dw_ks, stride=stride, dilation=dilation,
+                                      groups=dim_mid, norm_layer='bn_2d', act_layer='silu', inplace=inplace)
+        self.se = SqueezeExcite(dim_mid, rd_ratio=se_ratio,
+                                act_layer=get_act(act_layer)) if se_ratio > 0.0 else nn.Identity()
+
+        self.proj_drop = nn.Dropout(drop)
+        self.proj = ConvNormAct(dim_mid, dim_out, kernel_size=1, norm_layer='none', act_layer='none', inplace=inplace)
+        self.drop_path = DropPath(drop_path) if drop_path else nn.Identity()
+
+    def forward(self, x):
+        shortcut = x
+        x = self.norm(x)
+        if self.attn_s:
+            x = self.cbam(x)
+        else:
+            x = self.v(x)
+
+        x = x + self.se(self.conv_local(x)) if self.has_skip else self.se(self.conv_local(x))
+
+        x = self.proj_drop(x)
+        x = self.proj(x)
+
+        x = (shortcut + self.drop_path(x)) if self.has_skip else x
+        return x
+
+
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+
+class Conv(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.bn(self.conv(x)))
+
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        return self.act(self.conv(x))
+
+
+class Bottleneck(nn.Module):
+    """Standard bottleneck."""
+
+    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):
+        """Initializes a bottleneck module with given input/output channels, shortcut option, group, kernels, and
+        expansion.
+        """
+        super().__init__()
+        c_ = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, c_, k[0], 1)
+        self.cv2 = Conv(c_, c2, k[1], 1, g=g)
+        self.add = shortcut and c1 == c2
+        self.iRMB = iRMB_CBAM(c2)
+
+    def forward(self, x):
+        """'forward()' applies the YOLO FPN to input data."""
+        return x + self.iRMB(self.cv2(self.cv1(x))) if self.add else self.iRMB(self.cv2(self.cv1(x)))
+
+
+class C2f_iRMB_CBAM(nn.Module):
+    """Faster Implementation of CSP Bottleneck with 2 convolutions."""
+
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        """Initialize CSP bottleneck layer with two convolutions with arguments ch_in, ch_out, number, shortcut, groups,
+        expansion.
+        """
+        super().__init__()
+        self.c = int(c2 * e)  # hidden channels
+        self.cv1 = Conv(c1, 2 * self.c, 1, 1)
+        self.cv2 = Conv((2 + n) * self.c, c2, 1)  # optional act=FReLU(c2)
+        self.m = nn.ModuleList(Bottleneck(self.c, self.c, shortcut, g, k=((3, 3), (3, 3)), e=1.0) for _ in range(n))
+
+    def forward(self, x):
+        """Forward pass through C2f layer."""
+        y = list(self.cv1(x).chunk(2, 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+    def forward_split(self, x):
+        """Forward pass using split() instead of chunk()."""
+        y = list(self.cv1(x).split((self.c, self.c), 1))
+        y.extend(m(y[-1]) for m in self.m)
+        return self.cv2(torch.cat(y, 1))
+
+
+if __name__ == "__main__":
+    # Generating Sample image
+    image_size = (1, 64, 640, 640)
+    image = torch.rand(*image_size)
+
+    # Model
+    model = iRMB_CBAM(64)
+
+    out = model(image)
+    print(out.size())
